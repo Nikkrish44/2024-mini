16cd9f37fd638c72b74b96e89a69ae614a38b0c5
Nikhil <40872297+Nikkrish44@users.noreply.github.com>
2024-09-06 13:43:26 -0400

    Update exercise_sound.py

--- assignment/exercise_sound.py
+++ assignment/exercise_sound.py
@@ -30,10 +30,31 @@ duration: float = 0.1  # seconds
 
 print("Playing frequency (Hz):")
 
-for i in range(64):
+"""for i in range(64):
     print(freq)
     playtone(freq, duration)
     freq = int(freq * 1.1)
+"""
 
+bday  = [ 261, 261, 293, 261, 349, 329,
+          261, 261, 293, 261, 392, 349,
+          261, 261, 523, 440, 349, 329, 293,
+          466, 466, 440, 349, 392, 349]
+
+bdayT = [
+  .261, .250, .25, .25, .25, .5,
+  .25, .25, .25, .25, .25, .5,
+  .25, .25, .25, .25, .25, .25, 0.5,
+  .25, .25, .25, .25, .25, 0.5
+]
+
+playtone(8,1)
+
+for i in range(25):
+     print(bday[i])
+     playtone(bday[i], bdayT[i])
+     playtone(8,bdayT[i]*1.1)
+     
+    
 # Turn off the PWM
 quiet()
