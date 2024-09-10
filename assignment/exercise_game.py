faf76383509c357b32aa3da8e25b79edc317dea0
loucicala <160196133+loucicala@users.noreply.github.com>
2024-09-10 19:56:57 -0400

    Update exercise_game.py

--- assignment/exercise_game.py
+++ assignment/exercise_game.py
@@ -1,17 +1,41 @@
-"""
-Response time - single-threaded
-"""
-
-from machine import Pin
+import network # Only possible when installing the Pi Pico W version of MicroPython
 import time
-import random
+
+# CONNECT TO WIFI
+def connect_to_wifi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Connecting to network...')
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            time.sleep(1)
+    print('Network connected:', wlan.ifconfig())
+    
+connect_to_wifi('iPhone (3889)', 'password')
+
+import urequests
 import json
 
+# POST TO DATABASE
+def send_data_to_firebase(data):
+    url = 'https://ec463-miniproject-38c6e-default-rtdb.firebaseio.com/.json'
+    headers = {'Content-Type': 'application/json'}
+
+    try:
+        response = urequests.post(url, data=json.dumps(data), headers=headers)
+        print('Response:', response.text)
+        response.close()
+    except Exception as e:
+        print('Error:', e)
+
+# GAME CODE
+from machine import Pin
+import random
 
 N: int = 10
 sample_ms = 10.0
 on_ms = 500
-
 
 def random_time_interval(tmin: float, tmax: float) -> float:
     """return a random time interval between max and min"""
@@ -29,21 +53,9 @@ def blinker(N: int, led: Pin) -> None:
 
 
 def write_json(json_filename: str, data: dict) -> None:
-    """Writes data to a JSON file.
-
-    Parameters
-    ----------
-
-    json_filename: str
-        The name of the file to write to. This will overwrite any existing file.
-
-    data: dict
-        Dictionary data to write to the file.
-    """
 
     with open(json_filename, "w") as f:
         json.dump(data, f)
-
 
 def scorer(t: list[int | None]) -> None:
     # %% collate results
@@ -54,9 +66,7 @@ def scorer(t: list[int | None]) -> None:
 
     print(t_good)
 
-    # add key, value to this dict to store the minimum, maximum, average response time
-    # and score (non-misses / total flashes) i.e. the score a floating point number
-    # is in range [0..1]
+    # DICTIONARY WITH MIN, MAX, AVG, AND COUNT
     data = {}
     min_value = min(value for value in t if value is not None)
     max_value = max(value for value in t if value is not None)
@@ -80,10 +90,14 @@ def scorer(t: list[int | None]) -> None:
 
     write_json(filename, data)
 
+    # WHEN GAME IS COMPLETE, POST TO DATABASE
+    send_data_to_firebase(data)
+
 
 if __name__ == "__main__":
     # using "if __name__" allows us to reuse functions in other script files
 
+    # CHANGED TO PIN 15 FOR EASE OF USE -- 'LED' PIN DID NOTHING
     led = Pin(15, Pin.OUT)
     button = Pin(14, Pin.IN, Pin.PULL_UP)
 
